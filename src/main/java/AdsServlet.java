e1284cac77f67e5fc3eb94da5e55042eae666aee
John <cooks.johns@gmail.com>
2018-06-04 13:46:40 -0500

    dispaying adds

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -5,20 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet(name= "AdsServlet", urlPatterns = "/ads")
+@WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
 public class AdsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
-
-
-        if (request.getMethod().equalsIgnoreCase("post")) {
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            if (username.equals("admin") && password.equals("password")) {
-                response.sendRedirect("/profile");
-            }
-
-        }
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
 }
