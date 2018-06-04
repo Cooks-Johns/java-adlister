9f2e4bce20ebb288eef9fdd1214e8472aa7b63c0
John <cooks.johns@gmail.com>
2018-06-04 14:15:06 -0500

    dispaying adds

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -4,22 +4,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.rmi.activation.ActivationGroup_Stub;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")  // handles thes the ger request to /login and loads the page w/ the login form
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // render the .jsp and send the user that view
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
-
-        if (validAttempt) {
+        if( username.equals("admin") && password.equals("password")) {
             response.sendRedirect("/profile");
         } else {
-            response.sendRedirect("/login");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
         }
+
     }
 }
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -5,6 +5,7 @@
   Time: 11:00 AM
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib prefix="c" url="http://java.sun.com/jsp/jstl/core"%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
