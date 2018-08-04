7f883d274d39888344d477dde4f33aac109209d5
John <cooks.johns@gmail.com>
2018-08-03 19:53:08 -0500

    fixing the regisgter so that anyone can login coolBeans

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +13,14 @@ import java.io.IOException;
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO: show the registration form
+        // TODO: render the registration form to the user
         request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         // TODO: ensure the submitted information is valid
+
         // TODO: create a new user based off of the submitted information
         // TODO: if a user was successfully created, send them to their profile
         Boolean error = false;
@@ -24,6 +28,21 @@ public class RegisterServlet extends HttpServlet {
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        int numberOfRounds = 12;
+        String passwordConfirmation = request.getParameter("confirm_password");
+
+        boolean inputHasErrors = username.isEmpty()
+                || email.isEmpty()
+                || password.isEmpty()
+                || (! password.equals)passwordConfirmation));
+        if (inputHasErrors) {
+            response.sendRedirect("/register");
+            return;
         }
+
+        User user = User(username, email, password);
+        DaoFactory.getUserDao().insert(user);
+        response.sendRedirect("/login");
+
+        }
+
     }
